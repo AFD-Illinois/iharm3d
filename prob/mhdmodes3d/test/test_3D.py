69547f1e3344dd08ddee44e2c0417d80932a5056
Ben Prather <bprathr2@illinois.edu>
2017-11-16 18:43:27 -0600

    Convenience and reliability improvements for running 2D modes in 3D code

--- prob/mhdmodes3d/test/test_3D.py
+++ prob/mhdmodes3d/test/test_3D.py
@@ -29,7 +29,7 @@ for arg in sys.argv:
 RES = [16, 32, 64]#, 128]
 
 # LOOP OVER EIGENMODES
-MODES = [2] # 1,2,3
+MODES = [1,2,3]
 NAMES = ['ENTROPY', 'SLOW', 'ALFVEN', 'FAST']
 NVAR = 8
 VARS = ['rho', 'u', 'u1', 'u2', 'u3', 'B1', 'B2', 'B3']
@@ -41,7 +41,12 @@ k3 = 2.*np.pi
 var0 = np.zeros(NVAR)
 var0[0] = 1.
 var0[1] = 1.
+
+# Magnetic field
 var0[5] = 1.
+var0[6] = 0.
+var0[7] = 0.
+
 L1 = np.zeros([len(MODES), len(RES), NVAR])
 powerfits = np.zeros([len(MODES), NVAR])
 
@@ -71,29 +76,6 @@ for n in xrange(len(MODES)):
     dvar[5] = 0.359559114174
     dvar[6] = -0.359559114174
   dvar *= amp
-
-#   # EIGENMODES FAUX-2D
-#   dvar = np.zeros(NVAR)
-#   if MODES[n] == 0: # ENTROPY
-#     dvar[0] = 1.
-#   if MODES[n] == 1: # SLOW/SOUND
-#     dvar[0] = 0.558104461559
-#     dvar[1] = 0.744139282078
-#     dvar[2] = -0.277124827421
-#     dvar[3] = 0.0630348927707
-#     dvar[5] = -0.164323721928
-#     dvar[6] = 0.164323721928
-#   if MODES[n] == 2: # ALFVEN
-#     dvar[3] = 0.480384461415
-#     dvar[6] = 0.877058019307
-#   if MODES[n] == 3: # FAST
-#     dvar[0] = 0.476395427447
-#     dvar[1] = 0.635193903263
-#     dvar[2] = -0.102965815319
-#     dvar[3] = -0.316873207561
-#     dvar[5] = 0.359559114174
-#     dvar[6] = -0.359559114174
-#   dvar *= amp
 
 
   # USE DUMPS IN FOLDERS OF GIVEN FORMAT
@@ -128,44 +110,45 @@ for n in xrange(len(MODES)):
 
     mid = RES[m]/2
     
-    for fnum in xrange(len(np.sort(glob.glob('dump*.h5')))):
-      dfile = np.sort(glob.glob('dump*.h5'))[fnum]
-
-      hdr = io.load_hdr(dfile)
-      geom = io.load_geom(hdr, dfile)
-      dump = io.load_dump(hdr, geom, dfile)
-  
-      X1 = dump['x']
-      X2 = dump['y']
-      X3 = dump['z']
-  
-      dvar_code = []
-      dvar_code.append(dump['RHO'] - var0[0])
-      dvar_code.append(dump['UU'] - var0[1])
-      dvar_code.append(dump['U1'] - var0[2])
-      dvar_code.append(dump['U2'] - var0[3])
-      dvar_code.append(dump['U3'] - var0[4])
-      dvar_code.append(dump['B1'] - var0[5])
-      dvar_code.append(dump['B2'] - var0[6])
-      dvar_code.append(dump['B3'] - var0[7])
-      
-      dvar_plane = []
-      for k in xrange(NVAR):
-        dvar_plane.append(np.zeros((dump['N1'], dump['N2'])))
-        for i in xrange(dump['N1']):
-          for j in xrange(dump['N2']):
-            dvar_plane[k][i,j] = dvar_code[k][i,j,int(i/2 + j/2)]
-       
-      # Plot dvar
-      for k in xrange(NVAR):
-        if abs(dvar[k]) != 0.:
-          fig = plt.figure(figsize=(16.18,10))
-          ax = fig.add_subplot(1,1,1)
-          ax.pcolormesh(X1[:,:,mid], X2[:,:,mid], dvar_code[k][:,:,mid], label=VARS[k])
-          #ax.plot(X1[:,mid,mid], dvar_sol[k][:,mid,mid], marker='s', label=(VARS[k] + " analytic"))
-          plt.title(NAMES[MODES[n]] + ' ' + VARS[k] + ' ' + str(RES[m]))
-          plt.legend(loc=1)
-          plt.savefig('../test/modes_' + NAMES[MODES[n]] + '_' + VARS[k] + '_' + str(RES[m]) + '_' + str(fnum) + '.png', bbox_inches='tight')
+    # Plot slice at each timestep
+#     for fnum in xrange(len(np.sort(glob.glob('dump*.h5')))):
+#       dfile = np.sort(glob.glob('dump*.h5'))[fnum]
+# 
+#       hdr = io.load_hdr(dfile)
+#       geom = io.load_geom(hdr, dfile)
+#       dump = io.load_dump(hdr, geom, dfile)
+#   
+#       X1 = dump['x']
+#       X2 = dump['y']
+#       X3 = dump['z']
+#   
+#       dvar_code = []
+#       dvar_code.append(dump['RHO'] - var0[0])
+#       dvar_code.append(dump['UU'] - var0[1])
+#       dvar_code.append(dump['U1'] - var0[2])
+#       dvar_code.append(dump['U2'] - var0[3])
+#       dvar_code.append(dump['U3'] - var0[4])
+#       dvar_code.append(dump['B1'] - var0[5])
+#       dvar_code.append(dump['B2'] - var0[6])
+#       dvar_code.append(dump['B3'] - var0[7])
+#       
+#       dvar_plane = []
+#       for k in xrange(NVAR):
+#         dvar_plane.append(np.zeros((dump['N1'], dump['N2'])))
+#         for i in xrange(dump['N1']):
+#           for j in xrange(dump['N2']):
+#             dvar_plane[k][i,j] = dvar_code[k][i,j,int(i/2 + j/2)]
+#        
+#       # Plot dvar
+#       for k in xrange(NVAR):
+#         if abs(dvar[k]) != 0.:
+#           fig = plt.figure(figsize=(16.18,10))
+#           ax = fig.add_subplot(1,1,1)
+#           ax.pcolormesh(X1[:,:,mid], X2[:,:,mid], dvar_code[k][:,:,mid], label=VARS[k])
+#           #ax.plot(X1[:,mid,mid], dvar_sol[k][:,mid,mid], marker='s', label=(VARS[k] + " analytic"))
+#           plt.title(NAMES[MODES[n]] + ' ' + VARS[k] + ' ' + str(RES[m]))
+#           plt.legend(loc=1)
+#           plt.savefig('../test/modes_' + NAMES[MODES[n]] + '_' + VARS[k] + '_' + str(RES[m]) + '_' + str(fnum) + '.png', bbox_inches='tight')
 
   # MEASURE CONVERGENCE
   for k in xrange(NVAR):
