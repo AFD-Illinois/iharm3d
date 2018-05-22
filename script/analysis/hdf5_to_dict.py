Fix phi calculation in analysis

script/analysis/hdf5_to_dict.py
@@ -187,14 +187,14 @@ def load_dump(hdr, geom, diag, fname):
   dump['mdot'] = log_time(diag, 'mdot', dump['t'])
   gdet_shape = np.reshape(np.repeat(geom['gdet'][5,:],N3),(N2,N3))
   dump['Phi_calc'] = np.sum(np.abs(0.5*dump['B1'][5,:,:]*gdet_shape*hdr['dx2']*hdr['dx3']))
-  dump['phi_calc'] *= dump['Phi_calc']/(np.sqrt(dump['mdot']))
+  dump['phi_calc'] = dump['Phi_calc']/(np.sqrt(dump['mdot']))
   
   gdet_shape = np.reshape(np.repeat(geom['gdet'][:,N2/2],N3),(N1,N3))
   dump['Phi_disk'] = np.sum(np.abs(dump['B2'][:,N2/2,:]*gdet_shape*hdr['dx1']*hdr['dx3']))
   
   
   print "From Log: t: %f mdot: %f Phi_BH: %f" % (dump['t'], dump['mdot'], log_time(diag, 'phi', dump['t']))
-  print "Calculated: Phi_BH: %f Phi_disk: %f" % (dump['Phi_BH'], dump['Phi_disk'])
+  print "Calculated: phi_BH: %f Phi_disk: %f" % (dump['phi_calc'], dump['Phi_disk'])
 
   dump.update(geom)
   dump.update(hdr)
@@ -225,7 +225,7 @@ def load_log(logfile):
   
     diag['Phi'] = dfile[11]
     diag['phi'] = dfile[12]
-    diag['phi_calc'] = diag['Phi']/np.sqrt(diag['mdot'])
+    diag['phi_calc'] = diag['Phi'] / np.sqrt(diag['mdot'])
     diag['jet_EM_flux'] = dfile[13]
   
     diag['divbmax'] = dfile[14]