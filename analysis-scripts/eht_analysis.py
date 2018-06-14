Fix normalization of field line angular frequency

analysis-scripts/eht_analysis.py
@@ -160,7 +160,9 @@ def avg_dump(n):
   #Phi_bcon[n] = 0.5*(np.fabs(dump['bcon'][iF,:,:,1])*geom['gdet'][iF,:,None]*dx2*dx3).sum()
   # The HARM B_unit is sqrt(4pi)*c*sqrt(rho) so we need to multiply that in here
   Phi[n] = 0.5*(np.fabs(np.sqrt(4*np.pi)*dump['B1'][iEH,:,:])*geom['gdet'][iEH,:,None]*dx2*dx3).sum()
-  omega_th[n,:] = (dump['omega'][iEH,:,:].sum(axis=-1) + dump['omega'][iEH,::-1,:].sum(axis=-1))[:N2/2]
+  
+  omega_th[n,:] = dump['omega'][iEH,:N2/2,:].sum(axis=-1) + dump['omega'][iEH,:N2/2-1:-1,:].sum(axis=-1)
+  omega_th[n,:] /= 2*N3
   
   Mdot[n] = np.abs((dump['RHO'][iF,:,:]*dump['ucon'][iF,:,:,1]*geom['gdet'][iF,:,None]*dx2*dx3).sum())
   Trphi = (dump['RHO'] + dump['UU'] + (hdr['gam']-1.)*dump['UU'] + dump['bsq'])*dump['ucon'][:,:,:,1]*dump['ucov'][:,:,:,3]