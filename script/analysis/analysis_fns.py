Don't keep geom with dumps (too much memory). Fix normalization bugs for EHT comparison.

script/analysis/analysis_fns.py
@@ -9,21 +9,18 @@ THMAX = 2.*np.pi/3.
 
 ## Physics functions ##
 
-def Tcon(dump,i,j):
+def Tcon(geom, dump,i,j):
   gam = dump['hdr']['gam']
-  geom = dump['geom']
   return ( (dump['RHO'] + dump['UU'] + (gam-1)*dump['UU'] + dump['bsq'])*dump['ucon'][:,:,:,i]*dump['ucon'][:,:,:,j] +
            ((gam-1)*dump['UU'] + dump['bsq']/2)*geom['gcon'][:,:,None,i,j] - dump['bcon'][:,:,:,i]*dump['bcon'][:,:,:,j] )
 
-def Tcov(dump,i,j):
+def Tcov(geom, dump,i,j):
   gam = dump['hdr']['gam']
-  geom = dump['geom']
   return ( (dump['RHO'] + dump['UU'] + (gam-1)*dump['UU'] + dump['bsq'])*dump['ucov'][:,:,:,i]*dump['ucov'][:,:,:,j] +
            ((gam-1)*dump['UU'] + dump['bsq']/2)*geom['gcov'][:,:,None,i,j] - dump['bcov'][:,:,:,i]*dump['bcov'][:,:,:,j] )
 
-def Tmixed(dump,i,j):
+def Tmixed(geom, dump,i,j):
   gam = dump['hdr']['gam']
-  geom = dump['geom']
   gmixedij = (i == j)
   return ( (dump['RHO'] + dump['UU'] + (gam-1)*dump['UU'] + dump['bsq'])*dump['ucon'][:,:,:,i]*dump['ucov'][:,:,:,j] +
            ((gam-1)*dump['UU'] + dump['bsq']/2)*gmixedij - dump['bcon'][:,:,:,i]*dump['bcov'][:,:,:,j] )
@@ -32,7 +29,7 @@ def Tmixed(dump,i,j):
 # TODO there's a computationally easier way to do this:
 # Pre-populate an antisym ndarray and einsum
 # Same below
-def Fcon(dump, i, j):
+def Fcon(geom, dump, i, j):
   NDIM = dump['hdr']['n_dim']
   
   Fconij = np.zeros_like(dump['RHO'])
@@ -42,11 +39,10 @@ def Fcon(dump, i, j):
         Fconij[:,:,:] += _antisym(i,j,mu,nu) * dump['ucov'][:,:,:,mu] * dump['bcov'][:,:,:,nu]
 
   # TODO is normalization correct?
-  return Fconij*dump['geom']['gdet'][:,:,None]
+  return Fconij*geom['gdet'][:,:,None]
 
-def Fcov(dump, i, j):
+def Fcov(geom, dump, i, j):
   NDIM = dump['hdr']['n_dim']
-  geom = dump['geom']
 
   Fcovij = np.zeros_like(dump['RHO'])
   for mu in range(NDIM):
@@ -74,11 +70,11 @@ def sum_vol(geom, var, within=None):
 # TODO can I cache the volume here without a global or object?
 def eht_profile(geom, var, jmin, jmax):
   return ( np.sum(var[:,jmin:jmax,:] * geom['gdet'][:,jmin:jmax,None]*geom['dx2']*geom['dx3'], axis=(1,2)) /
-           (geom['dx2']*2.*np.pi*geom['gdet'][:,:]).sum(axis=-1) )
+           (geom['dx2']*2.*np.pi*geom['gdet'][:,jmin:jmax]).sum(axis=-1) )
 
-def theta_av(var, start, av):
-  # Sum theta from each pole to equator and take overall mean. N2 hack is a hack
-  N2 = var.shape[1]
+def theta_av(geom, var, start, av):
+  # Sum theta from each pole to equator and take overall mean
+  N2 = geom['n2']
   return (var[start:start+av,:N2//2,:].mean(axis=-1).mean(axis=0) + var[start:start+av,:N2//2-1:-1,:].mean(axis=-1).mean(axis=0)) / 2
 
 ## Internal functions ##

script/analysis/eht_analysis.py
@@ -120,8 +120,9 @@ def avg_dump(n):
     out['uphi_r'] = eht_profile(geom, uphi, jmin, jmax)
   
     # THETA AVERAGES
-    out['omega_th'] = theta_av(Fcov(dump, 0, 1), iEH, 1) / theta_av(Fcov(dump, 1, 3), iEH, 1)
-    out['omega_th_av'] = theta_av(Fcov(dump, 0, 1), iEH-2, 5) / theta_av(Fcov(dump, 1, 3), iEH-2, 5)
+    Fcov01, Fcov13 = Fcov(geom, dump, 0, 1), Fcov(geom, dump, 1, 3)
+    out['omega_th'] = theta_av(geom, Fcov01, iEH, 1) / theta_av(geom, Fcov13, iEH, 1)
+    out['omega_th_av'] = theta_av(geom, Fcov01, iEH-2, 5) / theta_av(geom, Fcov13, iEH-2, 5)
 
     # This produces much worse results
     #out['omega_th_alt'] = theta_av(Fcov(dump, 0, 2), iEH, 1) / theta_av(Fcov(dump, 2, 3), iEH, 1)
@@ -141,7 +142,7 @@ def avg_dump(n):
 
   # FLUXES
   # Radial profiles of Mdot and Edot, and their particular values
-  out['FE_r'] = sum_shell(geom, Tmixed(dump, 1,0))
+  out['FE_r'] = sum_shell(geom, Tmixed(geom, dump, 1,0))
   out['Edot'] = out['FE_r'][iF]
 
   out['FM_r'] = -sum_shell(geom, dump['RHO']*dump['ucon'][:,:,:,1])
@@ -152,7 +153,7 @@ def avg_dump(n):
   else:
     out['Mdot'] = out['FM_r'][iF]
 
-  out['Ldot'] = sum_shell(geom, Tmixed(dump, 1,3), at_zone=iF)
+  out['Ldot'] = sum_shell(geom, Tmixed(geom, dump, 1,3), at_zone=iF)
 
   # Maximum magnetization (and allow re-use of the variable)
   sigma = dump['bsq']/dump['RHO']
@@ -180,11 +181,12 @@ def avg_dump(n):
   j = rho**3 * P**(-2) * np.exp(-C*(rho**2 / (B*P**2))**(1./3.))
   out['Lum'] = sum_vol(geom, j)
 
-  out['Etot'] = sum_vol(geom, Tmixed(dump, 0, 0), within=iEmax)
+  T00 = Tmixed(geom, dump, 0,0)
+  out['Etot'] = sum_vol(geom, T00, within=iEmax)
   #print "Energy on grid: ",out['Etot']
 
   # For an averaged energy profile
-  #out['E_r'] = radial_sum(Tmixed(dump, 0,0))
+  #out['E_r'] = radial_sum(geom, T00)
 
   return out
 

script/analysis/eht_plot.py
@@ -31,12 +31,19 @@ if len(sys.argv) < 3:
   util.warn('Format: python eht_plot.py analysis_output [analysis_output ...] [labels_list] image_name')
   sys.exit()
 
-# All interior arguments are files to overplot
+# All interior arguments are files to overplot, except possibly the last
+if len(sys.argv) < 4:
+  last_file = -1
+else:
+  last_file = -2
+
+
 avgs = []
-for filename in sys.argv[1:-2]:
+for filename in sys.argv[1:last_file]:
   avgs.append(pickle.load(open(filename,'rb')))
 
-labels = sys.argv[-2].split(",")
+# Split the labels, or use the filename as a label
+labels = [ lab.replace("/",",") for lab in sys.argv[-2].split(",") ]
 
 if len(labels) < len(avgs):
   util.warn("Too few labels!")
@@ -49,15 +56,20 @@ ti = avgs[0]['t'][0]
 tf = avgs[0]['t'][-1]
 
 # Default styles
-styles = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
+if len(avgs) > 1:
+  styles = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
+else:
+  styles = ['k']
 
 def plot_all(ax, iname, varname, varname_pretty, logx=False, logy=False, ylim=None, timelabels=False):
   for i,avg in enumerate(avgs):
     ax.plot(avg[iname], avg[varname], styles[i], label=labels[i])
+  # Plot additions
   if logx: ax.set_xscale('log')
-  if logx: ax.set_yscale('log')
+  if logy: ax.set_yscale('log')
   if ylim is not None: ax.set_ylim(ylim)
   ax.grid()
+  ax.set_ylabel(varname_pretty)
   if iname == 't':
     ax.set_xlim([ti,tf])
     if timelabels:
@@ -68,17 +80,17 @@ def plot_all(ax, iname, varname, varname_pretty, logx=False, logy=False, ylim=No
     ax.set_xlabel(r"$\theta$")
   elif iname == 'r':
     ax.set_xlabel("r")
-    
 
 if RADS:
   fig, ax = plt.subplots(2,3, figsize=(FIGX, FIGY))
-  plot_all(ax[0], 'r', 'rho_r', r"$<\rho>$", logy=True, ylim=[1.e-2, 1.e0])
-  plot_all(ax, 'r', 'Pg_r', '<Pg>', logy=True, ylim=[1.e-6, 1.e-2])
-  plot_all(ax, 'r', 'B_r', '<|B|>', logy=True, ylim=[1.e-4, 1.e-1])
-  if len(avgs) > 1: ax.legend(loc=1)
-  plot_all(ax, 'r', 'uphi_r', '<u^phi>', logy=True, ylim=[1.e-3, 1.e1])
-  plot_all(ax, 'r', 'Ptot_r', '<Ptot>', logy=True, ylim=[1.e-6, 1.e-2])
-  plot_all(ax, 'r', 'betainv_r', '<beta^-1>', logy=True, ylim=[1.e-2, 1.e1])
+  plot_all(ax[0,0], 'r', 'rho_r', r"$<\rho>$", logy=True, ylim=[1.e-2, 1.e0])
+  plot_all(ax[0,1], 'r', 'Pg_r', '$<P_g>$', logy=True, ylim=[1.e-6, 1.e-2])
+  plot_all(ax[0,2], 'r', 'B_r', '$<|B|>$', logy=True, ylim=[1.e-4, 1.e-1])
+  plot_all(ax[1,0], 'r', 'uphi_r', '$<u^{\phi}>$', logy=True, ylim=[1.e-3, 1.e1])
+  plot_all(ax[1,1], 'r', 'Ptot_r', '$<P_{tot}>$', logy=True, ylim=[1.e-6, 1.e-2])
+  plot_all(ax[1,2], 'r', 'betainv_r', '$<\beta^{-1}>$', logy=True, ylim=[1.e-2, 1.e1])
+
+  if len(avgs) > 1: ax[0,2].legend(loc=1)
 
   plt.savefig(fname_out + '_ravgs.png')
   plt.close(fig)
@@ -90,86 +102,88 @@ if OMEGA:
   for avg in avgs:
     avg['omega_th'] *= 4/avg['a']
     avg['omega_th_av'] *= 4/avg['a']
-  plot_all(ax[0], 'th', 'omega_th', r"$\omega_f$ (EH, single shell)", ylim=[-1,2])
-  if len(avgs) > 1: ax[0].legend(loc=3)
-  
-  plot_all(ax[1], 'th', 'omega_th_av', r"$\omega_f$ (EH, 5-zone average)")
-  
+  plot_all(ax[0,0], 'th', 'omega_th', r"$\omega_f$ (EH, single shell)", ylim=[-1,2])
+  plot_all(ax[0,1], 'th', 'omega_th_av', r"$\omega_f$ (EH, 5-zone average)")
+
+  # Legend
+  if len(avgs) > 1: ax[0,0].legend(loc=3)
+
   # Horizontal guidelines
-  for a in ax:
+  for a in ax.flatten():
     a.axhline(0.5, linestyle='--', color='k')
-  
+
   plt.savefig(fname_out + '_omega.png')
   plt.close(fig)
 
 if FLUXES:
   fig,ax = plt.subplots(5,1, figsize=(FIGX, 5*PLOTY))
-  
+
   plot_all(ax[0],'t','Mdot', r"$|\dot{M}|$")
-  if len(avgs) > 1: ax.legend(loc=2)
-  
   plot_all(ax[1],'t','Phi', r"$\Phi$")
-  
+
   for avg in avgs:
     avg['abs_Ldot'] = np.abs(avg['Ldot'])
   plot_all(ax[2],'t','abs_Ldot', r"$|\dot{L}|$")
-  
+
   for avg in avgs:
     avg['EmM'] = np.fabs(np.fabs(avg['Edot']) - np.fabs(avg['Mdot']))
   plot_all(ax[3], 't', 'EmM', r"$|\dot{E} - \dot{M}|$")
-  
+
   plot_all(ax[4], 't', 'Lum', "Lum", timelabels=True)
-  
+
+  if len(avgs) > 1: ax[0].legend(loc=2)
+
   plt.savefig(fname_out + '_fluxes.png')
   plt.close(fig)
 
   fig, ax = plt.subplots(5,1, figsize=(FIGX, 5*PLOTY))
   plot_all(ax[0], 't', 'Mdot', r"$|\dot{M}|$")
   if len(avgs) > 1: ax.legend(loc=2)
-  
+
   for avg in avgs:
     avg['Phi_norm'] = avg['Phi']/np.sqrt(np.fabs(avg['Mdot']))
   plot_all(ax[1], 't', 'Phi_norm', r"$\frac{\Phi}{\sqrt{|\dot{M}|}}$")
-  
+
   for avg in avgs:
     avg['Ldot_norm'] = np.fabs(avg['Ldot'])/np.fabs(avg['Mdot'])
   plot_all(ax[2], 't', 'Ldot_norm', r"$\frac{|Ldot|}{|\dot{M}|}$")
-  
+
   for avg in avgs:
     avg['Edot_norm'] = np.fabs(np.fabs(avg['Edot']) - np.fabs(avg['Mdot']))/(np.fabs(avg['Mdot']))
   plot_all(ax[3], 't', 'Edot_norm', r"$\frac{|\dot{E} - \dot{M}|}{|\dot{M}|}$")
-  
+
   for avg in avgs:
     avg['Lum_norm'] = np.fabs(avg['Lum'])/np.fabs(avg['Mdot'])
   plot_all(ax[4], 't', 'Lum_norm', r"$\frac{Lum}{|\dot{M}|}$", timelabels=True)
-  
+
   plt.savefig(fname_out + '_normfluxes.png')
   plt.close(fig)
 
 if EXTRAS:
   fig, ax = plt.subplots(3,1, figsize=(FIGX, 3*PLOTY))
-  
+
   # Efficiency over mean mdot: just use the back half as <>
   for avg in avgs:
-    mdot_mean = np.mean(avg['Mdot'][len(avg['Mdot'])/2:])
+    mdot_mean = np.mean(avg['Mdot'][len(avg['Mdot'])//2:])
     avg['Eff'] = (avg['Mdot'] - avg['Edot'])/mdot_mean*100
   plot_all(ax[0], 't', 'Eff', "Efficiency (%)", ylim=[-10,200])
 
-  ax.plot(ax[1], 't', 'Edot', r"\dot{E}")
-  
+  plot_all(ax[1], 't', 'Edot', r"$\dot{E}$")
+
   for avg in avgs:
     avg['aLBZ'] = np.abs(avg['LBZ'])
-  ax.plot(ax[2], 't', 'aLBZ', "BZ Luminosity")
+  plot_all(ax[2], 't', 'aLBZ', "BZ Luminosity")
 
-  plt.savefig(fout + '_extras.png')
+  plt.savefig(fname_out + '_extras.png')
   plt.close(fig)
 
 if DIAGS:
   fig, ax = plt.subplots(3,1, figsize=(FIGX, FIGY/2))
+  ax = ax.flatten()
 
   plot_all(ax[0], 't', 'Etot', "Total E")
   plot_all(ax[1], 't', 'sigma_max', r"$\sigma_{max}$")
   plot_all(ax[2], 't_d', 'divbmax_d', "max divB")
 
-  plt.savefig(fout + '_diagnostics.png')
+  plt.savefig(fname_out + '_diagnostics.png')
   plt.close(fig)

script/analysis/hdf5_to_dict.py
@@ -95,7 +95,7 @@ def load_hdr(fname):
     
     # Grab the git revision if that's something we output
     if hdr['vnum'] >= [3,6]:
-      hdr['git_version'] = dfile['/extras/git_version'][()]
+      hdr['git_version'] = dfile['/extras/git_version'][()].decode('UTF-8')
   
   dfile.close()
   
@@ -113,7 +113,7 @@ def load_geom(hdr, path):
   geom = {}
   for key in list(gfile['/'].keys()):
     geom[key] = gfile[key][()]
-  
+
   # Useful stuff for direct access in geom. TODO r_isco if available
   for key in ['n1', 'n2', 'n3', 'dx1', 'dx2', 'dx3', 'startx1', 'startx2', 'startx3', 'n_dim']:
     geom[key] = hdr[key]
@@ -125,7 +125,7 @@ def load_geom(hdr, path):
   geom['x'] = geom['X']
   geom['y'] = geom['Y']
   geom['z'] = geom['Z']
-  
+
   # Compress geom in phi for normal use
   #geom['gdet_full'] = geom['gdet']
   geom['gdet'] = geom['gdet'][:,:,0]
@@ -142,9 +142,9 @@ def load_dump(fname, hdr, geom, derived_vars=True, extras=True):
   
   dump = {}
   
-  # Carry pointers to necessary external data, and hope it doesn't get copied...
+  # Carry pointers to header. Saves some pain getting shapes/parameters for plots
+  # Geometry, however, _must be carried separately_ due to size in memory
   dump['hdr'] = hdr
-  dump['geom'] = geom
 
   # TODO this necessarily grabs the /whole/ primitives array
   for key in [key for key in list(dfile['/'].keys()) if key not in ['header', 'extras', 'prims'] ]:
@@ -167,9 +167,9 @@ def load_dump(fname, hdr, geom, derived_vars=True, extras=True):
     dump['beta'] = 2.*(hdr['gam']-1.)*dump['UU']/(dump['bsq'])
 
     if hdr['has_electrons']:
-      ref = units.get_dict()
+      ref = units.get_cgs()
       dump['Thetae'] = ref['MP']/ref['ME']*dump['KEL']*dump['RHO']**(hdr['gam_e']-1.)
-      dump['ue'] = dump['KEL']*dump['RHO']**(hdr['gam_e'])/(hdr['gam_e']-1.)
+      dump['ue'] = dump['KEL']*dump['RHO']**(hdr['gam_e']) / (hdr['gam_e']-1.)
       dump['up'] = dump['UU'] - dump['ue']
       dump['TpTe'] = (hdr['gam_p']-1.)*dump['up']/((hdr['gam_e']-1.)*dump['ue'])
 
@@ -246,7 +246,7 @@ def get_state(hdr, geom, dump):
   ucon[:,:,:,2] = U2 - gamma*alpha*gcon[:,:,None,0,2]
   ucon[:,:,:,3] = U3 - gamma*alpha*gcon[:,:,None,0,3]
 
-  # TODO get the einsums working. Probably faster
+  # TODO get the einsums working as a lower() or raise() fn
   #ucov = np.einsum("ijka,ijkba->ijkb", ucon, gcov)
   for mu in range(NDIM):
     ucov[:,:,:,mu] = (ucon[:,:,:,:]*gcov[:,:,None,mu,:]).sum(axis=-1)

script/analysis/movie.py
@@ -65,14 +65,14 @@ def plot(n):
   if movie_type == "simplest":
     # Simplest movie: just RHO
     ax_slc = plt.subplots(1,2)
-    bplt.plot_slices(ax_slc[0], ax_slc[1], dump, np.log10(dump['RHO']),
+    bplt.plot_slices(ax_slc[0], ax_slc[1], geom, dump, np.log10(dump['RHO']),
                      label=r"$\log_{10}(\rho)$", vmin=rho_l, vmax=rho_h, window=window, cmap='jet')
   elif movie_type == "simpler":
     # Simpler movie: RHO and phi
     gs = gridspec.GridSpec(2, 2, height_ratios=[6, 1], width_ratios=[16,17])
     ax_slc = [fig.subplot(gs[0,0]), fig.subplot(gs[0,1])]
     ax_flux = [fig.subplot(gs[1,:])]
-    bplt.plot_slices(ax_slc[0], ax_slc[1], dump, np.log10(dump['RHO']),
+    bplt.plot_slices(ax_slc[0], ax_slc[1], geom, dump, np.log10(dump['RHO']),
                      label=r"$\log_{10}(\rho)$", vmin=rho_l, vmax=rho_h, window=window, cmap='jet')
     bplt.diag_plot(ax_flux[0], diag, 'phi', dump['t'], ylabel=r"$\phi_{BH}$", logy=LOG_PHI, xlabel=False)
   elif movie_type == "simple":
@@ -80,7 +80,7 @@ def plot(n):
     gs = gridspec.GridSpec(3, 2, height_ratios=[4, 1, 1])
     ax_slc = [fig.subplot(gs[0,0]), fig.subplot(gs[0,1])]
     ax_flux = [fig.subplot(gs[1,:]), fig.subplot(gs[2,:])]
-    bplt.plot_slices(ax_slc[0], ax_slc[1], dump, np.log10(dump['RHO']),
+    bplt.plot_slices(ax_slc[0], ax_slc[1], geom, dump, np.log10(dump['RHO']),
                      label=r"$\log_{10}(\rho)$", vmin=rho_l, vmax=rho_h, window=window, cmap='jet')
     bplt.diag_plot(ax_flux[0], diag, 'mdot', dump['t'], ylabel=r"$\dot{M}$", logy=LOG_MDOT)
     bplt.diag_plot(ax_flux[1], diag, 'phi', dump['t'], ylabel=r"$\phi_{BH}$", logy=LOG_PHI)
@@ -90,22 +90,22 @@ def plot(n):
     if movie_type == "traditional":
       # Usual movie: RHO beta fluxes
       # CUTS
-      bplt.plot_slices(ax_slc(1), ax_slc(2), dump, np.log10(dump['RHO']),
+      bplt.plot_slices(ax_slc(1), ax_slc(2), geom, dump, np.log10(dump['RHO']),
                        label=r"$\log_{10}(\rho)$", vmin=-3, vmax=2, cmap='jet')
-      bplt.plot_slices(ax_slc(5), ax_slc(6), dump, np.log10(dump['beta']),
+      bplt.plot_slices(ax_slc(5), ax_slc(6), geom, dump, np.log10(dump['beta']),
                        label=r"$\beta$", vmin=-2, vmax=2, cmap='RdBu_r')
       # FLUXES
       bplt.diag_plot(ax_flux(2), diag, 'mdot', dump['t'], ylabel=r"$\dot{M}$", logy=LOG_MDOT)
       bplt.diag_plot(ax_flux(4), diag, 'phi', dump['t'], ylabel=r"$\phi_{BH}$", logy=LOG_PHI)
       # Mixins:
       # Zoomed in RHO
-      bplt.plot_slices(ax_slc(7), ax_slc(8), dump, np.log10(dump['RHO']),
+      bplt.plot_slices(ax_slc(7), ax_slc(8), geom, dump, np.log10(dump['RHO']),
                        label=r"$\log_{10}(\rho)$", vmin=-3, vmax=2, window=[-10,10,-10,10], field_overlay=False)
       # Bsq
-#       bplt.plot_slices(ax_slc[6], ax_slc[7], dump, np.log10(dump['bsq']),
+#       bplt.plot_slices(ax_slc[6], ax_slc[7], geom, dump, np.log10(dump['bsq']),
 #                        label=r"$b^2$", vmin=-5, vmax=0, cmap='Blues')
       # Failures: all failed zones, one per nonzero pflag
-#       bplt.plot_slices(ax_slc[6], ax_slc[7], dump, dump['fail'] != 0,
+#       bplt.plot_slices(ax_slc[6], ax_slc[7], geom, dump, dump['fail'] != 0,
 #                        label="Failed zones", vmin=0, vmax=20, cmap='Reds', int=True) #, arrspace=True)
       # 2D histograms
 #       bplt.hist_2d(ax_slc[6], np.log10(dump['RHO']), np.log10(dump['UU']),r"$\log_{10}(\rho)$", r"$\log_{10}(U)$", logcolor=True)
@@ -115,21 +115,21 @@ def plot(n):
 #       bplt.diag_plot(ax_flux[1], diag, dump, 'edot', r"\dot{E}", logy=LOG_PHI)
     elif movie_type == "e_ratio":
       # Energy ratios: difficult places to integrate, with failures
-      bplt.plot_slices(ax_slc[0], ax_slc[1], dump, np.log10(dump['UU']/dump['RHO']),
+      bplt.plot_slices(ax_slc[0], ax_slc[1], geom, dump, np.log10(dump['UU']/dump['RHO']),
                        label=r"$\log_{10}(U / \rho)$", vmin=-3, vmax=3, average=True)
-      bplt.plot_slices(ax_slc[2], ax_slc[3], dump, np.log10(dump['bsq']/dump['RHO']),
+      bplt.plot_slices(ax_slc[2], ax_slc[3], geom, dump, np.log10(dump['bsq']/dump['RHO']),
                        label=r"$\log_{10}(b^2 / \rho)$", vmin=-3, vmax=3, average=True)
-      bplt.plot_slices(ax_slc[4], ax_slc[5], dump, np.log10(1/dump['beta']),
+      bplt.plot_slices(ax_slc[4], ax_slc[5], geom, dump, np.log10(1/dump['beta']),
                        label=r"$\beta^{-1}$", vmin=-3, vmax=3, average=True)
-      bplt.plot_slices(ax_slc[6], ax_slc[7], dump, dump['fail'] != 0,
+      bplt.plot_slices(ax_slc[6], ax_slc[7], geom, dump, dump['fail'] != 0,
                        label="Failures", vmin=0, vmax=20, cmap='Reds', int=True) #, arrspace=True)
     elif movie_type == "conservation":
       # Continuity plots to verify local conservation of energy, angular + linear momentum
       # Integrated T01: continuity for momentum conservation
-      bplt.plot_slices(ax_slc[0], ax_slc[1], dump, Tmixed(dump, 1, 0),
+      bplt.plot_slices(ax_slc[0], ax_slc[1], geom, dump, Tmixed(dump, 1, 0),
                        label=r"$T^1_0$ Integrated", vmin=0, vmax=600, arrspace=True, integrate=True)
       # integrated T00: continuity plot for energy conservation
-      bplt.plot_slices(ax_slc[4], ax_slc[5], dump, np.abs(Tmixed(dump, 0, 0)),
+      bplt.plot_slices(ax_slc[4], ax_slc[5], geom, dump, np.abs(Tmixed(dump, 0, 0)),
                        label=r"$T^0_0$ Integrated", vmin=0, vmax=3000, arrspace=True, integrate=True)
 
       # Usual fluxes for reference
@@ -137,25 +137,26 @@ def plot(n):
       #bplt.diag_plot(ax_flux[3], diag, 'phi', dump['t'], ylabel=r"$\phi_{BH}$", logy=LOG_PHI)
 
       # Radial conservation plots
-      E_r = sum_shell(geom,Tmixed(dump,0,0))
-      Ang_r = sum_shell(geom,Tmixed(dump,0,3))
+      E_r = sum_shell(geom,Tmixed(geom, dump, 0,0))
+      Ang_r = sum_shell(geom,Tmixed(geom, dump, 0,3))
       mass_r = sum_shell(dump['ucon'][:,:,:,0]*dump['RHO'])
 
       # TODO arrange legend better -- add labels when radial/diag plotting
-      bplt.radial_plot(ax_flux[3], np.abs(E_r), 'Conserved vars at R', ylim=(0,1000), rlim=(0,20), arrayspace=True)
-      bplt.radial_plot(ax_flux[3], np.abs(Ang_r)/10, '', ylim=(0,1000), rlim=(0,20), col='r', arrayspace=True)
-      bplt.radial_plot(ax_flux[3], np.abs(mass_r),   '', ylim=(0,1000), rlim=(0,20), col='b', arrayspace=True)
+      bplt.radial_plot(ax_flux[3], geom, np.abs(E_r), 'Conserved vars at R', ylim=(0,1000), rlim=(0,20), arrayspace=True)
+      bplt.radial_plot(ax_flux[3], geom, np.abs(Ang_r)/10, '', ylim=(0,1000), rlim=(0,20), col='r', arrayspace=True)
+      bplt.radial_plot(ax_flux[3], geom, np.abs(mass_r),   '', ylim=(0,1000), rlim=(0,20), col='b', arrayspace=True)
       
       # Radial energy accretion rate
-      Edot_r = sum_shell(geom, Tmixed(dump,1,0))
-      bplt.radial_plot(ax_flux[5], np.abs(Edot_r), 'Edot at R', ylim=(0,200), rlim=(0,20), arrayspace=True)
+      Edot_r = sum_shell(geom, Tmixed(geom, dump,1,0))
+      bplt.radial_plot(ax_flux[5], geom, np.abs(Edot_r), 'Edot at R', ylim=(0,200), rlim=(0,20), arrayspace=True)
 
       # Radial integrated failures
-      bplt.radial_plot(ax_flux[7], (dump['fail'] != 0).sum(axis=(1,2)), 'Fails at R', arrayspace=True, rlim=[0,50], ylim=[0,1000])
+      bplt.radial_plot(ax_flux[7], geom, (dump['fail'] != 0).sum(axis=(1,2)), 'Fails at R', arrayspace=True, rlim=[0,50], ylim=[0,1000])
 
     elif movie_type == "floors":
       # TODO add measures of all floors' efficacy.  Record ceilings in header or extras?
-      bplt.plot_slices('sigma ceiling', dump['bsq']/dump['RHO'] - 100, dump, -100, 100, 5, cmap='RdBu_r')
+      bplt.plot_slices(ax_flux[0], ax_flux[1], geom, dump['bsq']/dump['RHO'] - 100,
+                       vmin=-100, vmax=100, cmap='RdBu_r')
       bplt.diag_plot(ax, diag, dump, 'sigma_max', 'sigma_max')
 
   # TODO enlarge plots w/o messing up even pixel count

script/analysis/plot_init.py
@@ -56,7 +56,7 @@ if SIZE > geom['r'][-1,0,0]:
 fig = plt.figure(figsize=(FIGX, FIGY))
 # Density profile
 ax = plt.subplot(NPLOTSY,NPLOTSX,1)
-bplt.radial_plot(ax, dump, 'RHO', ylabel=r"$\rho$", n2=N2//2, n3=N3//2,
+bplt.radial_plot(ax, geom, dump, 'RHO', ylabel=r"$\rho$", n2=N2//2, n3=N3//2,
                  rlim=[8, 2*10**3], ylim=[10**(-4), 2], logr=True, logy=True)
 
 # B-flux thru midplane inside radius
@@ -68,31 +68,31 @@ for n in range(1,N1):
   flux_in[n] = flux_in[n-1] + np.sum(dump['B2'][n,N2//2,:]*geom['gdet'][n,N2//2,None]*hdr['dx1']*hdr['dx3'])
 
 ax = plt.subplot(NPLOTSY,NPLOTSX,2)
-bplt.radial_plot(ax, dump, flux_in, ylabel=r"Flux in r", rlim=[0, SIZE])
+bplt.radial_plot(ax, geom, dump, flux_in, ylabel=r"Flux in r", rlim=[0, SIZE])
 
 # Density 2D
 ax = plt.subplot(NPLOTSY,NPLOTSX,3)
-bplt.plot_xz(ax, dump, np.log10(dump['RHO']),
+bplt.plot_xz(ax, geom, dump, np.log10(dump['RHO']),
              vmin=-4, vmax = 0, label=r"$\log_{10}(\rho)$", window=[0,SIZE,-SIZE/2,SIZE/2])
 
 # Beta 2D
 ax = plt.subplot(NPLOTSY,NPLOTSX,4)
-bplt.plot_xz(ax, dump, np.log10(dump['beta']),
+bplt.plot_xz(ax, geom, dump, np.log10(dump['beta']),
              label=r"$\beta$", cmap='RdBu_r', vmin=1, vmax=4,
              window=[0,SIZE,-SIZE/2,SIZE/2])
-bplt.overlay_field(ax, dump, NLINES)
+bplt.overlay_field(ax, geom, dump, NLINES)
 
 if PLOT_EXTRA:
   ax = plt.subplot(NPLOTSY,NPLOTSX,5)
-  bplt.plot_xz(ax, dump, np.log10(dump['UU']),
+  bplt.plot_xz(ax, geom, dump, np.log10(dump['UU']),
                vmin=-4, vmax = 0, label=r"$\log_{10}(U)$",
                window=[0,SIZE,-SIZE/2,SIZE/2])
   
   ax = plt.subplot(NPLOTSY,NPLOTSX,6)
-  bplt.plot_xz(ax, dump, np.log10(dump['bsq']),
+  bplt.plot_xz(ax, geom, dump, np.log10(dump['bsq']),
                label=r"$\log_{10}(b^2)$", cmap='RdBu_r', vmin=-8, vmax=2,
                window=[0,SIZE,-SIZE/2,SIZE/2])
-  bplt.overlay_field(ax, dump, NLINES)
+  bplt.overlay_field(ax, geom, dump, NLINES)
 
 plt.tight_layout()
 

script/analysis/quick_plot.py
@@ -41,7 +41,8 @@ nplotsy = 2
 
 fig = plt.figure(figsize=(FIGX, FIGY))
 
-if var == 'jcov':
+# If we're plotting a derived variable, calculate + add it
+if var in ['jcov', 'jsq']:
   dump['jcov'] = np.zeros_like(dump['jcon'])
   for n in range(hdr['n_dim']):
     dump['jcov'][:,:,:,n] = np.sum(dump['jcon']*geom['gcov'][:,:,None,:,n], axis=3)
@@ -54,30 +55,20 @@ elif var == 'B':
   dump['B'] = np.sqrt(dump['bsq'])
 
 # Add units after all calculations, manually
-# M87
 if UNITS:
-  L_unit = 9.15766e+14
-  T_unit = 30546.6
-
-  ref = units.get_cgs()
-  RHO_unit = M_unit / (L_unit ** 3)
-  U_unit = RHO_unit*ref['CL']**2;
-  B_unit = ref['CL']*np.sqrt(4. * np.pi * RHO_unit)
-  Ne_unit = RHO_unit/(ref['MP'] + ref['ME'])
-  # TODO option for const
-  tp_over_te = 3
-  Thetae_unit = (hdr['gam']-1.)*ref['MP']/ref['ME']/(1. + tp_over_te)
+  unit = units.get_units_M87(M_unit, tp_over_te=3)
 
   if var in ['bsq']:
-    dump[var] *= B_unit**2
+    dump[var] *= unit['B_unit']**2
   elif var in ['B']:
-    dump[var] *= B_unit
-  elif var in ['Ne']: # Meaningless w/o units
-    dump[var] = dump['RHO'] * Ne_unit
+    dump[var] *= unit['B_unit']
+  elif var in ['Ne']:
+    dump[var] = dump['RHO'] * unit['Ne_unit']
   elif var in ['Te']:
-    dump[var] = ref['ME'] * ref['CL']**2 * Thetae_unit * dump['UU']/dump['RHO']
+    dump[var] = ref['ME'] * ref['CL']**2 * unit['Thetae_unit'] * dump['UU']/dump['RHO']
   elif var in ['Thetae']:
-    dump[var] = Thetae_unit * dump['UU']/dump['RHO']
+    # TODO non-const te
+    dump[var] = unit['Thetae_unit'] * dump['UU']/dump['RHO']
   # TODO the others
 
 
@@ -111,7 +102,7 @@ elif var in ['sigma']:
 elif var in ['bernoulli']:
   ax = plt.subplot(1, 1, 1)
   bplt.plot_xz(ax, dump[var], arrayspace=USEARRSPACE, average=True)
-  bplt.overlay_contour(ax, dump[var], [0.05])
+  bplt.overlay_contours(ax, dump[var], [0.05])
 else:
   ax = plt.subplot(1, 1, 1)
   bplt.plot_xz(ax, dump[var], arrayspace=USEARRSPACE)

script/analysis/units.py
@@ -1,4 +1,6 @@
-units = {
+## Handle adding units to quantities.  Work in progress
+
+cgs = {
 'CL' : 2.99792458e10,
 'QE' : 4.80320680e-10,
 'ME' : 9.1093826e-28,
@@ -20,7 +22,29 @@ units = {
 }
 
 def get_cgs():
-  return units
+  return cgs
+
+# Get M87 units. Pass tp_over_te=None to get non-constant-frac units
+def get_units_M87(M_unit, tp_over_te=3):
+  L_unit = 9.15766e+14
+  T_unit = 30546.6
+  return _get_all_units(M_unit, L_unit, T_unit, tp_over_te)
+
+# Internal method for all the well-defined units
+def _get_all_units(M_unit, L_unit, T_unit, tp_over_te):
+  out['M_unit'] = M_unit
+  out['L_unit'] = L_unit
+  out['T_unit'] = T_unit
+
+  RHO_unit = M_unit / (L_unit ** 3)
+  out['RHO_unit'] = RHO_unit
+  out['U_unit'] = RHO_unit*cgs['CL']**2;
+  out['B_unit'] = cgs['CL']*np.sqrt(4. * np.pi * RHO_unit)
+  out['Ne_unit'] = RHO_unit/(cgs['MP'] + cgs['ME'])
   
-def get_dict():
-  return units
+  if tp_over_te is not None:
+    out['Thetae_unit'] = (hdr['gam']-1.)*cgs['MP']/cgs['ME']/(1. + tp_over_te)
+  else:
+    out['Thetae_unit'] = cgs['MP']/cgs['ME']
+
+  return out