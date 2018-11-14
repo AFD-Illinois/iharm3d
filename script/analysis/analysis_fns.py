Fix nasty EHT disk-averaging bug, reshuffle some average and angle calculations

script/analysis/analysis_fns.py
@@ -117,15 +117,33 @@ def eht_vol(geom, var, jmin, jmax, outside=None):
   else:
     return np.sum(var[:,jmin:jmax,:] * geom['gdet'][:,jmin:jmax,None]*geom['dx1']*geom['dx2']*geom['dx3'])
 
-# TODO can I cache the volume here without a global or object?
+# TODO can I cache the volume instead of passing these?
+def get_j_vals(geom):
+  THMIN = np.pi/3.
+  THMAX = 2.*np.pi/3.
+  # Calculate jmin, jmax for EHT radial profiles
+  ths = geom['th'][-1,:,0]
+  for n in range(len(ths)):
+    if ths[n] > THMIN:
+      jmin = n
+      break
+  
+  for n in range(len(ths)):
+    if ths[n] > THMAX:
+      jmax = n
+      break
+
+  return jmin, jmax
+
+# TODO can I cache the volume instead of passing these?
 def eht_profile(geom, var, jmin, jmax):
-  return ( np.sum(var[:,jmin:jmax,:] * geom['gdet'][:,jmin:jmax,None]*geom['dx2']*geom['dx3'], axis=(1,2)) /
-           np.sum(geom['gdet'][:,jmin:jmax,None]*geom['dx2']*geom['dx3'], axis=(1,2)) )
+  return ( ((var[:,jmin:jmax,:] * geom['gdet'][:,jmin:jmax,None]).sum(axis=(1,2))*geom['dx2']*geom['dx3']) /
+           (geom['gdet'][:,jmin:jmax].sum(axis=1)*geom['dx2']*2*np.pi) )
 
 def theta_av(geom, var, start, av):
   # Sum theta from each pole to equator and take overall mean
   N2 = geom['n2']
-  return (var[start:start+av,:N2//2,:].mean(axis=-1).mean(axis=0) + var[start:start+av,:N2//2-1:-1,:].mean(axis=-1).mean(axis=0)) / 2
+  return (var[start:start+av,:N2//2,:].mean(axis=(0,2)) + var[start:start+av,:N2//2-1:-1,:].mean(axis=(0,2))) / 2
 
 ## Internal functions ##
 

script/analysis/eht_analysis.py
@@ -54,7 +54,7 @@ if tavg is None:
 # Decide where to measure fluxes
 def i_of(rcoord):
   i = 0
-  while geom['r'][i,0,0] < rcoord:
+  while geom['r'][i,hdr['n2']//2,0] < rcoord:
     i += 1
   i -= 1
   return i
@@ -73,18 +73,7 @@ iBZ = i_of(10) # TODO is there a standard measuring spot?
 # Some variables (Phi) should only be computed at EH=zone 5
 iEH = 5
 
-THMIN = np.pi/3.
-THMAX = 2.*np.pi/3.
-# Calculate jmin, jmax for EHT radial profiles
-ths = geom['th'][-1,:,0]
-for n in range(len(ths)):
-  if ths[n] > THMIN:
-    jmin = n
-    break
-for n in range(len(ths)):
-  if ths[n] > THMAX:
-    jmax = n
-    break
+jmin, jmax = get_j_vals(geom)
 
 # Variables which should be averaged
 avg_keys = ['rho_r', 'Theta_r', 'B_r', 'Pg_r', 'Ptot_r', 'betainv_r', 'uphi_r', 'FE_r', 'FM_r', 'omega_th', 'omega_th_av' ] #, 'omega_th_alt', 'omega_th_alt_av']
@@ -102,24 +91,16 @@ def avg_dump(n):
   if out['t'] > tavg:
 
     out['rho_r'] = eht_profile(geom, dump['RHO'], jmin, jmax)
-
-    Theta = (hdr['gam']-1.)*dump['UU']/dump['RHO']
-    out['Theta_r'] = eht_profile(geom, Theta, jmin, jmax)
-
-    B = np.sqrt(dump['bsq'])
-    out['B_r'] = eht_profile(geom, B, jmin, jmax)
+    out['Theta_r'] = eht_profile(geom, (hdr['gam']-1.)*dump['UU']/dump['RHO'], jmin, jmax)
+    out['B_r'] = eht_profile(geom, np.sqrt(dump['bsq']), jmin, jmax)
 
     Pg = (hdr['gam']-1.)*dump['UU']
+    Pb = dump['bsq']/2
     out['Pg_r'] = eht_profile(geom, Pg, jmin, jmax)
+    out['Ptot_r'] = eht_profile(geom, Pg + Pb, jmin, jmax)
+    out['betainv_r'] = eht_profile(geom, Pb/Pg, jmin, jmax)
 
-    Ptot = Pg + dump['bsq']/2
-    out['Ptot_r'] = eht_profile(geom, Ptot, jmin, jmax)
-
-    betainv = (dump['bsq']/2)/Pg
-    out['betainv_r'] = eht_profile(geom, betainv, jmin, jmax)
-
-    uphi = (dump['ucon'][:,:,:,3])
-    out['uphi_r'] = eht_profile(geom, uphi, jmin, jmax)
+    out['uphi_r'] = eht_profile(geom, dump['ucon'][:,:,:,3], jmin, jmax)
 
     # THETA AVERAGES
     Fcov01, Fcov13 = Fcov(geom, dump, 0, 1), Fcov(geom, dump, 1, 3)

script/analysis/movie.py
@@ -47,7 +47,7 @@ def plot(n):
 
   if movie_type not in ["simplest", "simpler", "simple"]:
     dump = io.load_dump(files[n], hdr, geom, derived_vars=True, extras=False)
-    fig.suptitle("t = %d"%dump['t']) # TODO put this at the bottom somehow?
+    #fig.suptitle("t = %d"%dump['t']) # TODO put this at the bottom somehow?
   else:
     # Simple movies don't need derived vars
     dump = io.load_dump(files[n], hdr, geom, derived_vars=False, extras=False)
@@ -87,18 +87,15 @@ def plot(n):
   elif movie_type == "radial":
 
     rho_r = eht_profile(geom, dump['RHO'], jmin, jmax)
-    Theta = (hdr['gam']-1.)*dump['UU']/dump['RHO']
-    Theta_r = eht_profile(geom, Theta, jmin, jmax)
-    B = np.sqrt(dump['bsq'])
-    B_r = eht_profile(geom, B, jmin, jmax)
+    B_r = eht_profile(geom, np.sqrt(dump['bsq']), jmin, jmax)
+    uphi_r = eht_profile(geom, dump['ucon'][:,:,:,3], jmin, jmax)
+    
     Pg = (hdr['gam']-1.)*dump['UU']
+    Pb = dump['bsq']/2
+    
     Pg_r = eht_profile(geom, Pg, jmin, jmax)
-    Ptot = Pg + dump['bsq']/2
-    Ptot_r = eht_profile(geom, Ptot, jmin, jmax)
-    betainv = (dump['bsq']/2)/Pg
-    betainv_r = eht_profile(geom, betainv, jmin, jmax)
-    uphi = (dump['ucon'][:,:,:,3])
-    uphi_r = eht_profile(geom, uphi, jmin, jmax)
+    Ptot_r = eht_profile(geom, Pg + Pb, jmin, jmax)
+    betainv_r = eht_profile(geom, Pb/Pg, jmin, jmax)
     
     ax_slc = lambda i: plt.subplot(2, 3, i)
     bplt.radial_plot(ax_slc(1), geom, rho_r, ylabel=r"$<\rho>$", logy=True, ylim=[1.e-2, 1.e0])
@@ -211,19 +208,8 @@ if __name__ == "__main__":
   hdr = io.load_hdr(files[0])
   geom = io.load_geom(hdr, path)
   
-  # TODO put this in analysis_fns
-  THMIN = np.pi/3.
-  THMAX = 2.*np.pi/3.
-  # Calculate jmin, jmax for EHT radial profiles
-  ths = geom['th'][-1,:,0]
-  for n in range(len(ths)):
-    if ths[n] > THMIN:
-      jmin = n
-      break
-  for n in range(len(ths)):
-    if ths[n] > THMAX:
-      jmax = n
-      break
+  jmin, jmax = get_j_vals(geom)
+  print("jmin: {} jmax: {}".format(jmin, jmax))
 
   if diag_post:
     # Load fluxes from post-analysis: more flexible