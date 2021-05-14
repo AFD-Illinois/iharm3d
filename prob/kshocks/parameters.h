546fa063cf0cc23656558e1b52421393c29e54f2
Ben Prather <bprathr2@illinois.edu>
2021-05-14 12:24:52 -0500

    Add remaining problems from iharm2d code (not the paper). Delete old bhlight problems that weren't useful.

--- prob/kshocks/parameters.h
+++ prob/kshocks/parameters.h
@@ -7,9 +7,9 @@
  ******************************************************************************/
 
 /* GLOBAL RESOLUTION */
-#define N1TOT 64
-#define N2TOT 64
-#define N3TOT 64
+#define N1TOT 512
+#define N2TOT 1
+#define N3TOT 1
 
 /* MPI DECOMPOSITION */
 /* DECOMPOSE IN N3 FIRST! Small leading array sizes for linear access */
@@ -22,23 +22,35 @@
  */
 #define METRIC MINKOWSKI
 
-#define NMODE 3
+/*
+ * FLOORS
+ * Wind term is a small source for torii only
+ * Maximum magnetization parameters should be set high for most problems
+ */
+#define WIND_TERM 0
+#define BSQORHOMAX (100.)
+#define UORHOMAX (100.)
 
-#define ELECTRONS 0
-
-#define RADIATION 0
+/* ELECTRONS AND OPTIONS
+ *   SUPPRESS_MAG_HEAT - (0,1) NO ELECTRON HEATING WHEN SIGMA > 1
+ *   BETA_HEAT         - (0,1) BETA-DEPENDENT HEATING
+ */
+#define ELECTRONS           0
+#define SUPPRESS_HIGHB_HEAT 1
+#define BETA_HEAT           1
 
 /* RECONSTRUCTION ALGORITHM
  *   LINEAR, PPM, WENO, MP5
  */
-#define RECONSTRUCTION WENO
+#define RECONSTRUCTION LINEAR
 
 /* BOUNDARY CONDITIONS
  *   OUTFLOW PERIODIC POLAR USER
  */
-#define X1L_BOUND PERIODIC
-#define X1R_BOUND PERIODIC
-#define X2L_BOUND PERIODIC
-#define X2R_BOUND PERIODIC
-#define X3L_BOUND PERIODIC
-#define X3R_BOUND PERIODIC
+#define X1L_BOUND OUTFLOW
+#define X1R_BOUND OUTFLOW
+#define X2L_BOUND OUTFLOW
+#define X2R_BOUND OUTFLOW
+#define X3L_BOUND OUTFLOW
+#define X3R_BOUND OUTFLOW
+
