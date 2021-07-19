ecfd510400a5de5b0ebfbbc69a88d7bcd040c967
Vedant Dhruv <43926431+vedantdhruv96@users.noreply.github.com>
2021-07-19 12:24:49 -0500

    Update phys.c

--- core/phys.c
+++ core/phys.c
@@ -219,7 +219,7 @@ inline void mhd_vchar(struct GridGeom *G, struct FluidState *S, int i, int j, in
   int loc, int dir, GridDouble cmax, GridDouble cmin)
 {
   double discr, vp, vm, bsq, ee, ef, va2, cs2, cms2, rho, u;
-  double Acov[NDIM], Bcov[NDIM], Acon[NDIM], Bcon[NDIM];which all 
+  double Acov[NDIM], Bcov[NDIM], Acon[NDIM], Bcon[NDIM]; 
   double Asq, Bsq, Au, Bu, AB, Au2, Bu2, AuBu, A, B, C;
 
   DLOOP1 {
