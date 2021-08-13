d6f73f51a4967efa8ed8170760376b8fd8d04f80
Vedant Dhruv <43926431+vedantdhruv96@users.noreply.github.com>
2021-08-13 11:48:19 -0500

    Update decs.h
    updated version. should assist in imaging

--- core/decs.h
+++ core/decs.h
@@ -36,7 +36,7 @@
       COMPILE-TIME PARAMETERS :
 *******************************************************************************/
 
-#define VERSION "iharm-alpha-3.6"
+#define VERSION "iharm-alpha-3.7"
 
 // Number of active zones on each MPI process
 #define N1       (N1TOT/N1CPU)
